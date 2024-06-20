58ec53ed97c169239b278bc0ddf6d56566a04cca
星空凌 <104693985+HosisoraLing@users.noreply.github.com>
2024-06-20 11:14:28 +0800

    Update theme.js

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -81,7 +81,7 @@
   $(window).on("load",function() {
     $( "#quick-search form" ).css('margin-right', $( "#s2id_project_quick_jump_box" ).width() + 60);
     $( 'input[name$="q"]' ).attr( 'placeholder','Enter Search Text' );
-    var pathname=window.location.pathname;
+    var pathname=window.location.pathname.split('/');
     console.log(pathname);
     if (activeStaticSidebar) {
       $( "#header").css("margin-left", "215px");
