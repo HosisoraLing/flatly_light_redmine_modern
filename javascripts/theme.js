659c5cb7fda7eea2ee30ef2007de46ebf383d504
星空凌 <104693985+HosisoraLing@users.noreply.github.com>
2024-06-21 09:09:50 +0800

    Update theme.js

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -86,10 +86,10 @@
    var homeLink = document.querySelector('a.home');
    // 修改href属性
     homeLink.href = "http://mx.yinhe596.cn:40880/projects/our-home/wiki";
-    var url=window.location.href;
-    if (url=="http://mx.yinhe596.cn:40880/"){
-      window.location.href = "http://mx.yinhe596.cn:40880/projects/our-home/wiki";
-    }
+    // var url=window.location.href;
+    // if (url=="http://mx.yinhe596.cn:40880/"){
+    //   window.location.href = "http://mx.yinhe596.cn:40880/projects/our-home/wiki";
+    // }
     var pathname=window.location.pathname.split('/');
     console.log(pathname);
     if(pathname.includes("attachments")){
