7a6f7c8adab07c91c3eeff23517d84a5fa9a5152
Hosisora_Ling <2374416274@qq.com>
2024-09-11 11:08:49 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	javascripts/theme.js

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -213,7 +213,7 @@
           // 获取画布的尺寸
           var canvasWidth = canvas.width;
           var canvasHeight = canvas.height;
-          // 将像素转换为点（假设 1pt = 1px）
+          // 将像素转换为点
           var pdfWidth = canvasWidth / 1;
           var pdfHeight = canvasHeight / 1;
           // 创建 PDF 实例，方向设置为横向（landscape）
@@ -221,7 +221,7 @@
           // 添加图像到 PDF，宽度和高度设置为 PDF 页面的宽度和高度减去边距
           pdf.addImage(imgData, 'PNG', margin, margin, pdfWidth - 2 * margin, pdfHeight - 2 * margin);
           // 保存 PDF
-          pdf.save('table.pdf');
+          pdf.save('gantt-table.pdf');
         });
       });
 
